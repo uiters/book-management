169e4825243497bbcf2de7d3c1b9ac2cc99eaefb
ANDY <ANDY@DESKTOP-60EFS58>
2021-02-19 15:16:03 +0700

    Change user model

--- book-management-be/book-management-models/Models/User.cs
+++ book-management-be/book-management-models/Models/User.cs
@@ -7,12 +7,13 @@ namespace book_management_models
     {
         public string Name { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateofBirth { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public ICollection<Review> Reviews { get; set; }
         public Cart Cart { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
